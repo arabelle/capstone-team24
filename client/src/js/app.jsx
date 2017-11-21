5c8a536e4db375d56fbf787eaafc0a87ed841344
Jay Dahiya <e1x8@ece.ubc.ca>
2017-11-20 23:43:27 -0800

    Redid CSS and updated code for Navbar

--- client/src/js/app.jsx
+++ client/src/js/app.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {Navbar} from './navbar.jsx';
 import styles from '../css/index.css';
 
 export class Header extends React.Component {
   render () {
       return(
       <div>
+        <Navbar/>
         <h1 className="bellRings"> Why is the Bell Ringing? </h1>
       </div>
       );
