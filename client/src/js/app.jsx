990d59fd213ad6ab10b4bd4e4b5a6ce3379a3aa2
Arabelle <arabelle310@yahoo.ca>
2017-11-20 22:33:13 -0800

    Fix encoding for html

--- client/src/js/app.jsx
+++ client/src/js/app.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import {render} from 'react-dom';
 
+const Entities = require('html-entities').AllHtmlEntities;
+const entities = new Entities();
+var decoded = entities.decode(events);
+
 export class Header extends React.Component {
   render () {
       return(
       <div>
         <h1 className="bellRings"> Why is the Bell Ringing? </h1>
-        <h2 className="mainEvent"> {events} </h2>
+        <h2 className="mainEvent"> {decoded} </h2>
       </div>
       );
     }
--- crawler.py
+++ crawler.py
@@ -2,6 +2,9 @@ import newspaper
 import db
 from newspaper import news_pool
 
+news_websites = ['https://www.nytimes.com/']
+# 'http://www.huffingtonpost.ca/','http://www.cnn.com/world'
+
 def download_articles(news_websites):
     papers = []
     for site in news_websites:
@@ -14,14 +17,18 @@ def download_articles(news_websites):
 
 def get_relevant_articles(papers):
     for paper in papers:
-        for article in articles:
-            pass
+        for article in paper.articles:
+            insert_db(article)
 
 def insert_db(article):
-    date = article.publish_date if article.publish_date is not None else ""
+    date = article.publish_date.strftime("%Y-%m-%d %H:%M:%S") if article.publish_date is not "" else ""
     imgLink = article.top_image if article.top_image is not None else ""
     link = article.url
-    title = article.title
+    title = article.title.replace("'", r"\'").replace('"', r'\"')
+    print("Date: ")
+    print(article.publish_date)
+    print("Url: " + article.url)
+    print("Title: " + article.title)
     summary = get_summary(article.text)
     db.insertIntoTable(date, title, summary, link, imgLink)
 
@@ -30,12 +37,14 @@ def get_summary(text):
     # Get first 4 sentences
     for sentence in text.split('.')[:5]:
         summary += sentence + " "
-    return summary
+    return text.replace("'", r"\'").replace('"', r'\"')
 
+papers = download_articles(news_websites)
+get_relevant_articles(papers)
 #news_websites = ['http://www.nytimes.com', 'http://www.huffingtonpost.ca/','http://www.cnn.com/world']
 #nyt_paper = newspaper.build('http://www.nytimes.com')
 #huff_paper = newspaper.build('http://www.huffingtonpost.ca/')
-google_paper = newspaper.build("http://vancouversun.com/")
+"""google_paper = newspaper.build("http://vancouversun.com/")
 
 print("SOURCE: {}".format(google_paper.brand))
 print("Google News: {} articles\n".format(len(google_paper.articles)))
@@ -43,7 +52,7 @@ print("Google News: {} articles\n".format(len(google_paper.articles)))
 article = google_paper.articles[0]
 article.download()
 insert_db(article)
-"""print("Url: " + article.url)
+print("Url: " + article.url)
 article.parse()
 print("Title: " + article.title)
 print(article.text)
