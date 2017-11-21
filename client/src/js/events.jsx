5da7884f289e3e3af73d95872fb408d45dd64e2f
Jay Dahiya <e1x8@ece.ubc.ca>
2017-11-20 22:34:35 -0800

    Updated events file

--- client/src/js/events.jsx
+++ client/src/js/events.jsx
@@ -4,9 +4,15 @@ import styles from '../css/index.css';
 
 var eventsList = events;
 
-export class Events extends React.Component{
-    
-    
+export class MainEvent extends React.Component{
+    render(){
+        return (
+        <h2>Canucks Win</h2>
+        );
+    }
+}
+
+export class EventsList extends React.Component{
     render(){
         return (
         <h2>{eventsList}</h2>
--- client/src/js/index.jsx
+++ client/src/js/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {Header} from './app.jsx';
+import {EventsList, MainEvent} from './events.jsx';
  
-render(<Header/>, document.getElementById('app'));
+render(<div><Header/><MainEvent/><EventsList/></div>, document.getElementById('app'));
