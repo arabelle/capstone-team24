b45af3b452c4252a12b741e6ab21c2da1aa4f4eb
Jay Dahiya <e1x8@ece.ubc.ca>
2017-11-20 22:53:42 -0800

    Changed css for webpage to follow a template

--- client/src/js/index.jsx
+++ client/src/js/index.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import {render} from 'react-dom';
-import {Header} from './app.jsx';
-import {EventsList, MainEvent} from './events.jsx';
- 
-render(<div><Header/><MainEvent/><EventsList/></div>, document.getElementById('app'));
+import { render } from 'react-dom';
+import { Header } from './app.jsx';
+import { EventsList, MainEvent } from './events.jsx';
+
+render(<div>
+    <Header />
+    <MainEvent />
+    <EventsList />
+</div>, document.getElementById('app'));
